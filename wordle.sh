67ea4dfa9fd3987d03f0b6c6ae8818f4b6bee924
breadleaf <bkhnapa@gmail.com>
2024-02-23 14:33:50 -0700

    update lauch file

--- wordle.sh
+++ wordle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 install_py_text="Error: Python3 is not installed, please install and try again";
-help_text="usage: $(basename $0) (optional: DEBUG | HELP)";
+help_text="usage: $(basename $0) [ RUN | DEBUG | HELP ]";
 
 if command -v python3 > /dev/null 2>&1; then
 	echo -n "";
@@ -10,8 +10,8 @@ else
 fi
 
 if [ $# -eq 0 ]; then
-	DEBUG= python3 wordle.py;
-	exit "$?";
+	echo "$help_text";
+	exit 1;
 fi
 
 if [ $# -gt 1 ]; then
@@ -20,6 +20,10 @@ if [ $# -gt 1 ]; then
 fi
 
 case $1 in
+	"RUN")
+		python3 wordle.py;
+		exit "$?";
+		;;
 	"DEBUG")
 		python3 wordle.py DEBUG;
 		exit "$?";
